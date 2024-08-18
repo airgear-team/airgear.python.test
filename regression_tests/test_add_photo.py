6df5739d5947fc23df3f6f23a140118270082990
Oleksandr <okrasnodubets@gmail.com>
2024-08-18 12:39:41 +0200

    adding with auth and without tests added

---
+++ regression_tests/test_add_photo.py
@@ -0,0 +1,35 @@
+import unittest
+
+import requests
+
+# URL для тестів
+base_url = "http://localhost:8080"
+
+# Bearer Token для автентифікації
+bearer_token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJncmlpaWluNDEuZG9kZTcxQGV4YW1wbGUuY29tIiwicm9sZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE3MjM5NzQzNDEsImV4cCI6MTcyMzk5MjM0MX0.lp7aEpPAY9L5FcS5elUDQAh4jk9ArJgu5517gqU1JVI"
+
+
+class TestImageAPI(unittest.TestCase):
+
+    def test_add_image_without_auth(self):
+        url = f"{base_url}/images/1"
+        files = {'images': open('earth.jpg', 'rb')}
+
+        response = requests.post(url, files=files)
+
+        # Перевіряємо, чи був повернутий очікуваний код статусу
+        self.assertEqual(response.status_code, 404, f"Expected status code 404, but got {response.status_code}")
+
+    def test_add_image_with_auth(self):
+        url = f"{base_url}/images/1"
+        with open('earth.jpg', 'rb') as image_file:
+            files = {'images': ('earth.jpg', image_file, 'image/jpeg')}
+
+            headers = {
+                'Authorization': f'Bearer {bearer_token}'
+            }
+
+            response = requests.post(url, files=files, headers=headers)
+
+            # Перевіряємо, чи був повернутий очікуваний код статусу
+            self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code}")
