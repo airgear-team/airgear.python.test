7e74fd6df188b5d2d97a48d4e73c192f8d3d616d
Oleksandr <okrasnodubets@gmail.com>
2024-08-23 19:46:43 +0200

    get image by id test added

--- regression_tests/RegressionGetPhotoId/test_get_photo_id.py
+++ regression_tests/RegressionGetPhotoId/test_get_photo_id.py
@@ -5,7 +5,7 @@ import requests
 base_url = "http://localhost:8080"
 
 # Bearer Token для автентифікації
-bearer_token = "your_bearer_token" # Замість "your_bearer_token" вставте реальний токен
+bearer_token = "your_bearer_token"  # Замість "your_bearer_token" вставте реальний токен
 
 
 class TestGetPhotos(unittest.TestCase):
@@ -28,6 +28,26 @@ class TestGetPhotos(unittest.TestCase):
 
         for image in response_data:
             self.assertIn('imageUrl', image, "Missing 'imageUrl' in response JSON.")
+
+    def get_image_by_id(self):
+
+        image_ids = [
+            "4537ab99-6652-4c79-a207-cc91c87c4fb2",
+            "2a59abd9-6fa9-4605-80cd-7381ce98e90d",
+            "53d0292e-cf2e-4896-bd49-4823d83fe8cd"
+        ]
+
+        for image_id in image_ids:
+            url = f"{base_url}/images/{image_id}.jpg"
+
+            headers = {
+                'Authorization': f'Bearer {bearer_token}'
+            }
+
+            response = requests.get(url, headers=headers)
+
+            # Перевірка на очікуваний код статусу
+            self.assertEqual(response.status_code, 200, f'Expected 200, but got {response.status_code}')
 
 
 if __name__ == "__main__":
