12a7b00fef17e9b7f5addfac10f9d9267b18a67a
sccssfllf <okrasnodubets@gmail.com>
2024-09-09 18:49:49 +0200

    unsuccsessful auth test added

--- integrating_tests/IntegrationRegistAndAuth/test_auth.py
+++ integrating_tests/IntegrationRegistAndAuth/test_auth.py
@@ -5,17 +5,25 @@ import requests
 class TestAuthenticateUser(unittest.TestCase):
     BASE_URL = "http://localhost:8080/auth"
 
-    def test_authenticate_user(self):
+    @classmethod
+    def setUpClass(cls):
+        print("\nПідготовка до тестування автентифікації...")
+
+    def test_successful_authenticate_user(self):
+
+        print("\nТестуємо успішну автентифікацію.")
+
         # Дані для аутентифікації зареєстрованого користувача
         auth_payload = {
-            "email": "griiiin41.dode71@example.com",
-            "password": "secretpassword2"
+            "email": "your_email", # Вставте правильну пошту замість your_email
+            "password": "your_password" # Вставте правильний пароль замість your_password
         }
 
         headers = {
             'Content-Type': 'application/json'
         }
 
+        print(f"Виконуємо POST-запит на URL: {self.BASE_URL}/authenticate, з правильними даними.")
         # Виконуємо POST-запит для аутентифікації
         response = requests.post(f"{self.BASE_URL}/authenticate", json=auth_payload, headers=headers)
 
@@ -25,7 +33,33 @@ class TestAuthenticateUser(unittest.TestCase):
         # Перевіряємо, що у відповіді є токен
         response_data = response.json()
         self.assertIn("token", response_data, "Response JSON does not contain 'token'")
-        print(f"Authentication successful, token received: {response_data['token']}")
+        print(f"\nАвтентифікація успішна, отримано токен: {response_data['token']}")
+
+
+    def test_unsuccessful_authenticate_user(self):
+
+        print("\nТестуємо безуспішну автентифікацію.")
+
+        # Хибні дані для аутентифікації зареєстрованого користувача
+
+        auth_payload = {
+            "email": "wrong@email.com",  # правильну пошту
+            "password": "all_wrong"  # неправильний пароль
+        }
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        print(f"Виконуємо POST-запит на URL: {self.BASE_URL}/authenticate, з хибними даними.")
+        # Виконуємо POST-запит для аутентифікації
+        response = requests.post(f"{self.BASE_URL}/authenticate", json=auth_payload, headers=headers)
+
+        # Перевіряємо, що статус-код відповіді є 200 (OK)
+        self.assertEqual(response.status_code, 401, "Expected status code 401, got {0}".format(response.status_code))
+
+    def tearDown(self):
+        print("Тест завершено.\n")
 
 
 if __name__ == '__main__':
