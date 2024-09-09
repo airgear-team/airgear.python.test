90be172e56a43c0d1968ec85c13f8495db4def8c
sccssfllf <okrasnodubets@gmail.com>
2024-09-09 18:56:47 +0200

    minor style changes

--- integrating_tests/IntegrationRegistAndAuth/test_register.py
+++ integrating_tests/IntegrationRegistAndAuth/test_register.py
@@ -1,10 +1,16 @@
 import unittest
+from multiprocessing.reduction import register
+
 import requests
 from mail_num_generator import Generator
 
 
 class TestRegisterUser(unittest.TestCase):
     BASE_URL = "http://localhost:8080/auth"
+
+    @classmethod
+    def setUpClass(cls):
+        print("\nПідготовка до тестування реєстрації...")
 
     def test_register_user(self):
         # Дані для реєстрації нового користувача
@@ -14,6 +20,7 @@ class TestRegisterUser(unittest.TestCase):
             "phone": f"{Generator.generate_num()}",
             "name": "test test"
         }
+        print(f"\nПроведення тесту з реєстрації із реєстраційними даними: {register_payload}")
 
         headers = {
             'Content-Type': 'application/json'
@@ -21,6 +28,7 @@ class TestRegisterUser(unittest.TestCase):
 
         # Виконуємо POST-запит для реєстрації
         response = requests.post(f"{self.BASE_URL}/register", json=register_payload, headers=headers)
+        print(f"Виконуємо POST-запит для реєстрації за посиланням: {self.BASE_URL}/register")
 
         # Перевіряємо, що статус-код відповіді є 201 (OK)
         self.assertEqual(response.status_code, 201, "Expected status code 201, got {0}".format(response.status_code))
@@ -28,7 +36,11 @@ class TestRegisterUser(unittest.TestCase):
         # Перевіряємо, що у відповіді є токен
         response_data = response.json()
         self.assertIn("token", response_data, "Response JSON does not contain 'token'")
-        print(f"Registration successful, token received: {response_data['token']}")
+        print(f"Реєстрація вдала, отримано токен: {response_data['token']}")
+
+    @classmethod
+    def tearDownClass(cls):
+        print(f"Тест завершено.")
 
 
 if __name__ == '__main__':
