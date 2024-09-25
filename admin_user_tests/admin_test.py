8eaa2155e766bda7454fe5071a64ffcdbf154229
sccssfllf <okrasnodubets@gmail.com>
2024-09-25 13:01:45 +0200

    category test

--- admin_user_tests/admin_test.py
+++ admin_user_tests/admin_test.py
@@ -11,7 +11,7 @@ class TestAdminUser(unittest.TestCase):
 
     # Базове посилання
     BASE_URL = 'http://localhost:8082'
-    ADMIN_TOKEN = 'your_token_here'  # Вставте правильний токен тут
+    ADMIN_TOKEN = 'token_here'  # Вставте правильний токен тут
 
     def test_return_goods_count(self):
         print(f"\ntest_return_goods_count почато")
@@ -23,6 +23,7 @@ class TestAdminUser(unittest.TestCase):
 
         response = requests.get(URL, headers=headers)
 
+        # Перевіряємо статус-код відповіді
         print(f"Отримано відповідь з кодом статусу: {response.status_code}")
         self.assertEqual(200, response.status_code,
                          f'Очікуваний статус-код 200, отримано {response.status_code}')
@@ -33,6 +34,8 @@ class TestAdminUser(unittest.TestCase):
         response_data = response.json()
         for count in response_data:
             self.assertIn('count', count, "Бракує \'count\' у JSON файлі")
+
+        # Перевіряємо, що відповідь містить очікувану структуру
         if len(response_data) == 0:
             print("Файл пустий")
         else:
@@ -48,6 +51,7 @@ class TestAdminUser(unittest.TestCase):
 
         response = requests.get(URL, headers=headers)
 
+        # Перевіряємо статус-код відповіді
         print(f"Отримано відповідь з кодом статусу: {response.status_code}")
         self.assertEqual(200, response.status_code,
                          f'Очікуваний статус-код 200, отримано {response.status_code}')
@@ -58,6 +62,8 @@ class TestAdminUser(unittest.TestCase):
         response_data = response.json()
         for count in response_data:
             self.assertIn('count', count, "Бракує \'count\' у JSON файлі")
+
+        # Перевіряємо, що відповідь містить очікувану структуру
         if len(response_data) == 0:
             print("Файл пустий")
         else:
@@ -72,6 +78,7 @@ class TestAdminUser(unittest.TestCase):
 
         response = requests.get(URL, headers=headers)
 
+        # Перевіряємо статус-код відповіді
         print(f"Отримано відповідь з кодом статусу: {response.status_code}")
         self.assertEqual(200, response.status_code,
                          f'Очікуваний статус-код 200, отримано {response.status_code}')
@@ -82,6 +89,8 @@ class TestAdminUser(unittest.TestCase):
         response_data = response.json()
         for count in response_data:
             self.assertIn('count', count, "Бракує \'count\' у JSON файлі")
+
+        # Перевіряємо, що відповідь містить очікувану структуру
         if len(response_data) == 0:
             print("Файл пустий")
         else:
@@ -96,6 +105,7 @@ class TestAdminUser(unittest.TestCase):
 
         response = requests.get(URL, headers=headers)
 
+        # Перевіряємо статус-код відповіді
         print(f"Отримано відповідь з кодом статусу: {response.status_code}")
         self.assertEqual(200, response.status_code,
                          f'Очікуваний статус-код 200, отримано {response.status_code}')
@@ -107,10 +117,45 @@ class TestAdminUser(unittest.TestCase):
 
         self.assertIn('count', response_data, "Бракує \'count\' у JSON файлі")
         self.assertIn('name', response_data, "Бракує \'count\' у JSON файлі")
+
+        # Перевіряємо, що відповідь містить очікувану структуру
         if len(response_data) == 0:
             print("Файл пустий")
         else:
             print(f"Вміст файлу: {response_data} \n")
+
+    def test_return_count_category(self):
+        print("\ntest_return_count_category почато")
+
+        URL = f'{self.BASE_URL}/admin/goods/count/category'  # Вставте власний формат дати
+        headers = {
+            'Authorization': f'Bearer {self.ADMIN_TOKEN}'
+        }
+
+        response = requests.get(URL, headers=headers)
+
+        # Перевіряємо статус-код відповіді
+        print(f"Отримано відповідь з кодом статусу: {response.status_code}")
+        self.assertEqual(200, response.status_code,
+                         f'Очікуваний статус-код 200, отримано {response.status_code}')
+
+        self.assertTrue(response.headers['Content-type'].startswith('application/json'),
+                        'Файл не відповідає розширенню JSON')
+
+        # Перевіряємо, що відповідь містить очікувану структуру
+        response_data = response.json()
+
+        self.assertIn('content', response_data, "Бракує \'content\' у JSON файлі")
+        self.assertIsInstance(response_data['content'], list, "\'content\' має бути типу list")
+
+        self.assertGreater(len(response_data['content']), 0, "\'Content\' не має бути порожнім")
+
+        first_category = response_data['content'][0]
+        self.assertIn('name', first_category, "\'name\' поле не знайдено в категоріях")
+        self.assertIn('count', first_category, "\'count\' поле не знайдено в категоріях")
+
+        self.assertIsInstance(first_category['name'], str, "\'name\' має бути string")
+        self.assertIsInstance(first_category['count'], int, "\'count\' має бути integer")
 
     @classmethod
     def tearDownClass(cls):
